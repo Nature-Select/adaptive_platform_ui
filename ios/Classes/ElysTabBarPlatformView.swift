193f49454ecef32ee11dcc1a16bc1fea75861092
yuanbo <yuanbo@natureselect.ai>
2026-03-06 17:09:14 +0800

    chore: 增大图标

--- ios/Classes/ElysTabBarPlatformView.swift
+++ ios/Classes/ElysTabBarPlatformView.swift
@@ -206,7 +206,7 @@ class ElysTabBarPlatformView: NSObject, FlutterPlatformView, UITabBarDelegate {
     private func createTabItem(index: Int, icon: String, selectedIcon: String, badgeCount: Int?) -> UITabBarItem {
         var image: UIImage?
         var selectedImage: UIImage?
-        let iconSize = CGSize(width: 26, height: 26)
+        let iconSize = CGSize(width: 40, height: 40)
         let iconUseTemplate = shouldUseTemplateRendering(icon)
         let selectedIconUseTemplate = shouldUseTemplateRendering(selectedIcon)
 
@@ -672,7 +672,7 @@ class ElysTabBarPlatformView: NSObject, FlutterPlatformView, UITabBarDelegate {
 
             let icon = args["icon"] as? String
             let selectedIcon = args["selectedIcon"] as? String
-            let iconSize = CGSize(width: 26, height: 26)
+            let iconSize = CGSize(width: 40, height: 40)
 
             // Update stored icons
             if let icon = icon, index < self.currentIcons.count {
